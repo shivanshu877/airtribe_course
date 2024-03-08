a24fd583574b81ee397ddfefad22ced970b404ac
shivanshu877 <vashudev8777@gmail.com>
2024-03-08 14:06:46 +0530

    /api/course/create api added

--- index.js
+++ index.js
@@ -5,6 +5,48 @@ const port = 3000;
 const fs = require("fs");
 const pool = require("./pool");
 app.use(bodyParser.json());
+var validator = require("validator");
+
+app.post("/api/course/create", async (req, res) => {
+  const { name, max_seats, start_date, instructor_id } = req.body;
+  if (!name || !max_seats || !start_date || !instructor_id) {
+    return res
+      .status(400)
+      .send("name, max_seats, start_date, instructor_id required");
+  }
+  var parts = start_date.split("-");
+  var dt = new Date(
+    parseInt(parts[2], 10),
+    parseInt(parts[1], 10) - 1,
+    parseInt(parts[0], 10)
+  );
+  if (dt == "Invalid date" || isNaN(dt) || dt < new Date()) {
+    return res
+      .status(400)
+      .send(
+        "start_date required in the future date format and in mm-dd-yyyy format"
+      );
+  }
+
+  console.log(name, max_seats, start_date, instructor_id);
+  try {
+    const query = `
+      INSERT INTO courses (name, max_seats, start_date, instructor_id)
+      VALUES ($1, $2, $3 , $4 )
+      RETURNING id;
+    `;
+    const values = [name, max_seats, start_date, instructor_id];
+
+    const result = await pool.query(query, values);
+    res
+      .status(201)
+      .send({ message: "New course created", courseId: result.rows[0].id });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ err });
+  }
+});
+
 app.post("/setup", async (req, res) => {
   try {
     const backupData = fs.readFileSync("./setup_script.txt").toString("utf-8");
@@ -12,16 +54,6 @@ app.post("/setup", async (req, res) => {
     // Create the database
     await pool.query(backupData);
     res.status(200).json({ message: "Backup successful" });
-  } catch (error) {
-    console.error("Error performing backup:", error);
-    res.status(500).json({ error: "Backup failed" });
-  }
-});
-
-app.get("/instructors", async (req, res) => {
-  try {
-    const instructors = await pool.query("SELECT * FROM instructors");
-    res.status(200).json(instructors.rows);
   } catch (error) {
     console.error("Error performing backup:", error);
     res.status(500).json({ error: "Backup failed" });
--- pool.js
+++ pool.js
@@ -1,10 +1,23 @@
+// local machine setup
 const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
-  host: "db",
+  host: "localhost",
   database: "postgres",
-  password: "postgres",
+  password: "pattern",
   port: 5432,
 });
 module.exports = pool;
+
+//for docker
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "db",
+//   database: "postgres",
+//   password: "postgres",
+//   port: 5432,
+// });
+// module.exports = pool;
